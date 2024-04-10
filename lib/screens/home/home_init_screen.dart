8ee5fd0faca256e688addc09095438aca67c0b8a
CBGoncalves <carlos.eduardo46851@gmail.com>
2024-04-10 20:29:39 -0300

    separando construcao da pagina da chamada

---
+++ lib/screens/home/home_init_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pushschool/components/preview_card.dart';
+import '../../components/railmenu.dart';
+
+class HomeInitScreen extends StatelessWidget {
+  const HomeInitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Olá, Carlos',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.message))
+        ],
+      ),
+      body: Row(
+        children: [
+          const RailMenu(selectedDrawerIndex: 0),
+          const SizedBox(width: 6),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Novidades para você",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                PreviewCard(
+                  image: 'assets/img/logica_programacao.png',
+                  icon: Icons.subscriptions,
+                  title: 'Lógica de Programação',
+                  subtitle: 'loren ipsun domain loren ipsun domain',
+                  label: 'Ver Mais',
+                  onPressed: () {
+                    context.push('/home');
+                  },
+                ),
+                PreviewCard(
+                  image: 'assets/img/seguranca.png',
+                  icon: Icons.article,
+                  title:
+                      'OWASP Top 10 - Sua aplicação é realmente segura? Descubra...',
+                  subtitle: 'loren ipsun domain loren ipsun domain',
+                  label: 'Ver Mais',
+                  onPressed: () {
+                    context.push('/home');
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:pushschool/components/preview_card.dart';
-import 'package:pushschool/components/railmenu.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pushschool/screens/home/home_init_screen.dart';
 import 'package:pushschool/screens/login/login_screen.dart';
 
 import 'bloc/home_bloc.dart';
@@ -33,59 +31,7 @@ class _HomeViewState extends State<HomeView> {
       listener: (context, state) {},
       builder: (context, state) {
         if (state is HomePageOpened) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Olá, Carlos',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {},
-                ),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.message))
-              ],
-            ),
-            body: Row(
-              children: [
-                const RailMenu(selectedDrawerIndex: 0),
-                const SizedBox(width: 6),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Novidades para você",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      PreviewCard(
-                        image: 'assets/img/logica_programacao.png',
-                        icon: Icons.subscriptions,
-                        title: 'Lógica de Programação',
-                        subtitle: 'loren ipsun domain loren ipsun domain',
-                        label: 'Ver Mais',
-                        onPressed: () {
-                          context.push('/home');
-                        },
-                      ),
-                      PreviewCard(
-                        image: 'assets/img/seguranca.png',
-                        icon: Icons.article,
-                        title:
-                            'OWASP Top 10 - Sua aplicação é realmente segura? Descubra...',
-                        subtitle: 'loren ipsun domain loren ipsun domain',
-                        label: 'Ver Mais',
-                        onPressed: () {
-                          context.push('/home');
-                        },
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          );
+          return const HomeInitScreen();
         }
         return const LoginScreen();
       },
